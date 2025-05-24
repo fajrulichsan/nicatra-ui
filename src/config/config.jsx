56dcb75b099af413d5670b381020f9d02b5530d9
psmfajru1107 <fajrul.ichsankamil@ai.astra.co.id>
2025-05-24 13:25:12 +0700

    base url prod

--- src/config/config.jsx
+++ src/config/config.jsx
@@ -1,5 +1,6 @@
 const config = {
-    BASE_URL: 'http://localhost:3100',  
+    // BASE_URL: 'http://localhost:3100',  
+    BASE_URL: 'http://localhost:3100/api/',  
   };
   
   export default config;
